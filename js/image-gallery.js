de0ad3633ee9350e7fb03f9459b388654b7f342c
mehulsatardekar <mehulsatardekar1999@gmail.com>
2021-08-10 22:47:23 +0530

    image gallery example

---
+++ js/image-gallery.js
@@ -0,0 +1,15 @@
+let images = document.getElementsByClassName('panel');
+
+console.log(images);
+
+
+Array.from(images).map(image => image.addEventListener('click',()=>{
+    console.log('works fine mate');
+    image.classList.toggle('addflex');
+}))
+
+// images[0].addEventListener('click',()=>{
+    
+//     images[0].classList.toggle('addflex')
+
+// })
